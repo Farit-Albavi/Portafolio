e75aba8edfff18d4517e38d2e22475bcd16d472c
faritdev <albavi.farit2@gmail.com>
2023-05-23 20:10:45 -0400

    add

--- main.js
+++ main.js
@@ -1,27 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const enlaces = document.querySelectorAll("nav ul li a");
-    const secciones = document.querySelectorAll("section");
-
-    function cambiarColorEnlace() {
-        const scrollPos = window.scrollY;
-
-        secciones.forEach(function (seccion) {
-            const seccionTop = seccion.offsetTop;
-            const seccionHeight = seccion.offsetHeight;
-
-            if (scrollPos >= seccionTop && scrollPos < seccionTop + seccionHeight) {
-                const targetEnlace = document.querySelector('a[href="#' + seccion.getAttribute("id") + '"]');
-                enlaces.forEach(function (enlace) {
-                    enlace.classList.remove("nav-active");
-                });
-                targetEnlace.classList.add("nav-active");
-            }
-        });
-    }
-
-    window.addEventListener("scroll", cambiarColorEnlace);
-});
-
 // Función para desplazarse hasta la parte superior de la página
 function scrollToTop() {
     window.scrollTo({
@@ -53,3 +29,91 @@ const sendmail = document.getElementById("sendmail");
 sendmail.addEventListener('submit', (e) => {
     e.preventDefault();
 })
+
+//Esta funcionalidad hace dinamico la barra de navegación
+
+let farit = document.getElementById("itsme");
+let aboutme = document.getElementById("abtme");
+let habilidades = document.getElementById("skills");
+let educacion = document.getElementById("ed");
+let portfolio = document.getElementById("porfolio");
+let contacto = document.getElementById("contact");
+
+
+
+function esVisible(seccion) {
+    var posicion = seccion.getBoundingClientRect();
+    var ventanaAltura = window.innerHeight;
+
+    return (
+        posicion.top >= 0 &&
+        posicion.bottom <= ventanaAltura
+    );
+}
+
+const li = document.querySelectorAll('a');
+
+function manejarScroll() {
+
+    //HACER MAS CORTO ESTE CODIGO
+
+    if (esVisible(farit)) {
+        li[0].classList.add('active')
+
+    } else {
+        li[0].classList.remove('active')
+    }
+
+    if (esVisible(aboutme)) {
+        li[1].classList.add('active')
+
+    } else {
+        li[1].classList.remove('active')
+    }
+
+    if (esVisible(habilidades)) {
+        li[2].classList.add('active')
+
+    } else {
+        li[2].classList.remove('active')
+    }
+
+    if (esVisible(educacion)) {
+        li[3].classList.add('active')
+    } else {
+        li[3].classList.remove('active')
+    }
+
+    if (esVisible(portfolio)) {
+        li[4].classList.add('active')
+
+    } else {
+        li[4].classList.remove('active')
+    }
+
+    if (esVisible(contacto)) {
+        li[5].classList.add('active')
+
+    } else {
+        li[5].classList.remove('active')
+    }
+
+
+
+}
+
+var posicionYAnterior = window.pageYOffset || document.documentElement.scrollTop;
+window.addEventListener('scroll', function () {
+    var posicionYActual = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (posicionYActual > posicionYAnterior) {
+        manejarScroll()
+        // El usuario ha hecho scroll hacia abajo
+    } else if (posicionYActual < posicionYAnterior) {
+        manejarScroll()
+        // El usuario ha hecho scroll hacia arriba
+    }
+
+    // Actualizar la posición Y anterior
+    posicionYAnterior = posicionYActual;
+});
